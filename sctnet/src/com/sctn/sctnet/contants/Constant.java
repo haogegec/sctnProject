18e4bd8fe6e7cfc4dbe4ca214643dcc09120a013
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-07 21:10:12 +0800

    dd

---
+++ sctnet/src/com/sctn/sctnet/contants/Constant.java
@@ -0,0 +1,9 @@
+package com.sctn.sctnet.contants;
+
+public class Constant {
+
+	public static String category = "";//美容：cosmetic、美发：barber
+	public static String ServerQrcodeImageURL="http://www.weeqii.com/booking/main!getQrcodeImg";
+	public static String ServerURL="http://www.weeqii.com/booking/";//公司测试环境地址  阿里云
+	public static String ServerImageURL="http://www.weeqii.com/booking/main!getImage";//公司测试环境地址
+}
