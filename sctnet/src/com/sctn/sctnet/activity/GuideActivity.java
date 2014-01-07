5c1260a37973ece222123396f1e92226e7c3a99e
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-07 19:22:51 +0800

    WWW

---
+++ sctnet/src/com/sctn/sctnet/activity/GuideActivity.java
@@ -0,0 +1,17 @@
+package com.sctn.sctnet.activity;
+
+import com.sctn.sctnet.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuideActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.guide_activity);
+		
+	}
+
+}
