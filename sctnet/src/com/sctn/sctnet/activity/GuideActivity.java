f88968e8e73e745a96479b91d45716d60f6ee8fb
wanghao <96011864z@gmail.com>
2014-01-08 19:31:52 +0800

    添加注释了。。。。

--- sctnet/src/com/sctn/sctnet/activity/GuideActivity.java
+++ sctnet/src/com/sctn/sctnet/activity/GuideActivity.java
@@ -10,6 +10,7 @@ public class GuideActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//你好你好你好
         setContentView(R.layout.guide_activity);
 		
 	}
