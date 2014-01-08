b096b67440e3ef351a6367c80337035a28b8a646
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-08 21:54:37 +0800

    ss

--- sctnet/src/com/sctn/sctnet/activity/GuideActivity.java
+++ sctnet/src/com/sctn/sctnet/activity/GuideActivity.java
@@ -10,12 +10,7 @@ public class GuideActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//你好你好你好
-		
-		//我好我好
-		
-		
         setContentView(R.layout.guide_activity);
-		
+        
 	}
 }
