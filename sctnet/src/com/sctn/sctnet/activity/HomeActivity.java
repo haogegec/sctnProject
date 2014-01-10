ae152c66a3294e1c0bf28e1287f65d219ca2dd1f
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-10 20:35:01 +0800

    ddd

--- sctnet/src/com/sctn/sctnet/activity/HomeActivity.java
+++ sctnet/src/com/sctn/sctnet/activity/HomeActivity.java
@@ -3,15 +3,21 @@ package com.sctn.sctnet.activity;
 
 import com.sctn.sctnet.R;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends BaicActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		this.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);// 设置标题栏为用户自定义标题栏
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_activity);
+		setTitleBar(getString(R.string.homeActivityTitle), View.VISIBLE, View.VISIBLE);
+		
+		
+		
 	}
 
 }
