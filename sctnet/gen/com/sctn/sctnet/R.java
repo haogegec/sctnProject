8d4576e7a31b6faf8e3845c0596b90b222d0a831
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-10 20:35:53 +0800

    ddd

--- sctnet/gen/com/sctn/sctnet/R.java
+++ sctnet/gen/com/sctn/sctnet/R.java
@@ -11,6 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        /**  透明色  
+         */
+        public static final int homePageBg=0x7f040001;
         public static final int transparent=0x7f040000;
     }
     public static final class dimen {
@@ -19,36 +22,57 @@ public final class R {
         public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
-        public static final int btn_guide_start=0x7f020000;
-        public static final int guide_four=0x7f020001;
-        public static final int guide_one=0x7f020002;
-        public static final int guide_three=0x7f020003;
-        public static final int guide_two=0x7f020004;
-        public static final int ic_action_search=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int loading=0x7f020007;
-        public static final int loading_progress=0x7f020008;
-        public static final int titlebar_bg=0x7f020009;
-        public static final int titlebar_img_btn_back_normal=0x7f02000a;
-        public static final int titlebar_img_btn_back_pressed=0x7f02000b;
-        public static final int titlebar_img_btn_left_bg=0x7f02000c;
-        public static final int titlebar_img_btn_reload_normal=0x7f02000d;
-        public static final int titlebar_img_btn_reload_pressed=0x7f02000e;
-        public static final int titlebar_img_btn_right_bg=0x7f02000f;
-        public static final int welcome=0x7f020010;
+        public static final int background=0x7f020000;
+        public static final int btn_guide_start=0x7f020001;
+        public static final int document_query=0x7f020002;
+        public static final int function_more=0x7f020003;
+        public static final int guide_four=0x7f020004;
+        public static final int guide_one=0x7f020005;
+        public static final int guide_three=0x7f020006;
+        public static final int guide_two=0x7f020007;
+        public static final int ic_action_search=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int infomation_query=0x7f02000a;
+        public static final int job_search_bg=0x7f02000b;
+        public static final int loading=0x7f02000c;
+        public static final int loading_progress=0x7f02000d;
+        public static final int person_centre_bg=0x7f02000e;
+        public static final int rem_meeting=0x7f02000f;
+        public static final int resume_manage=0x7f020010;
+        public static final int salary_survey=0x7f020011;
+        public static final int search_bar=0x7f020012;
+        public static final int search_bg=0x7f020013;
+        public static final int titlebar_bg=0x7f020014;
+        public static final int titlebar_img_btn_back_normal=0x7f020015;
+        public static final int titlebar_img_btn_back_pressed=0x7f020016;
+        public static final int titlebar_img_btn_left_bg=0x7f020017;
+        public static final int titlebar_img_btn_reload_normal=0x7f020018;
+        public static final int titlebar_img_btn_reload_pressed=0x7f020019;
+        public static final int titlebar_img_btn_right_bg=0x7f02001a;
+        public static final int welcome=0x7f02001b;
+        public static final int work_direction_bg=0x7f02001c;
     }
     public static final class id {
         public static final int btn_guide_start=0x7f090002;
-        public static final int button1=0x7f090003;
-        public static final int empty_view=0x7f090004;
+        public static final int empty_view=0x7f09000d;
+        public static final int home_activity=0x7f090003;
+        public static final int home_activity_onerow=0x7f090007;
+        public static final int home_activity_search=0x7f090004;
+        public static final int home_activity_threerow=0x7f09000c;
+        public static final int home_activity_tworow=0x7f09000b;
+        public static final int job_search=0x7f090009;
         public static final int ll=0x7f090001;
-        public static final int menu_settings=0x7f09000a;
-        public static final int progress_text=0x7f090005;
-        public static final int titlebar_img_btn_left=0x7f090006;
-        public static final int titlebar_img_btn_right=0x7f090009;
-        public static final int titlebar_text=0x7f090008;
-        public static final int titlebar_title=0x7f090007;
+        public static final int menu_settings=0x7f090013;
+        public static final int person_centre=0x7f090008;
+        public static final int progress_text=0x7f09000e;
+        public static final int search_bar=0x7f090005;
+        public static final int search_bg=0x7f090006;
+        public static final int titlebar_img_btn_left=0x7f09000f;
+        public static final int titlebar_img_btn_right=0x7f090012;
+        public static final int titlebar_text=0x7f090011;
+        public static final int titlebar_title=0x7f090010;
         public static final int vp_guide_viewpager=0x7f090000;
+        public static final int work_direction=0x7f09000a;
     }
     public static final class layout {
         public static final int guide_activity=0x7f030000;
@@ -67,6 +91,12 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060001;
+        /**      主页 
+         */
+        public static final int homeActivityTitle=0x7f060007;
+        public static final int homeactivityhint=0x7f060008;
+        /**      deloge 
+         */
         public static final int loading=0x7f060006;
         public static final int menu_settings=0x7f060002;
         public static final int title_activity_main=0x7f060003;
