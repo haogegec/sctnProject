29eada8606c9919c4103157593355fb60ba7fb36
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-12 18:12:39 +0800

    ff

--- sctnet/src/com/sctn/sctnet/activity/WorkSearchActivity.java
+++ sctnet/src/com/sctn/sctnet/activity/WorkSearchActivity.java
@@ -4,7 +4,6 @@ import com.sctn.sctnet.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 
 public class WorkSearchActivity extends Activity {
