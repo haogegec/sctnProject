e6aa2524adc55fff252e0c0a5418d1152cf0be51
wanghaoc <wanghaoc@N-WANGHAOC.cattsoft.com>
2014-01-19 23:39:47 +0800

    qqq

--- sctnet/src/com/sctn/sctnet/activity/BaicActivity.java
+++ sctnet/src/com/sctn/sctnet/activity/BaicActivity.java
@@ -1,7 +1,22 @@
 package com.sctn.sctnet.activity;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+
 import com.sctn.sctnet.R;
+import com.sctn.sctnet.Utils.EncryptUtil;
+import com.sctn.sctnet.Utils.StringUtil;
+import com.sctn.sctnet.Utils.SysException;
 import com.sctn.sctnet.cache.SctnAplication;
+import com.sctn.sctnet.contants.Constant;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -24,11 +39,12 @@ public  abstract class BaicActivity  extends Activity{
 	private SharedPreferences sharedPreferences;
 	private ImageView titleLeftButton;// 标题栏左边按钮，默认图标为返回，背景为透明
 	private TextView titleTextView;// 标题栏居中标题
-	private ImageView titleRightButton;// 标题栏右边文字，默认图标为刷新，背景为透明
+	protected ImageView titleRightButton;// 标题栏右边文字，默认图标为刷新，背景为透明
 	
 	public Dialog mProgressDialog;// 进度条
 	public boolean isCancel = false;// 判断是按了返回键
 	private String progressText;// 进度条显示文字，null则取默认值
+	private String encryptUsable;
 
 	
 	@Override
@@ -37,6 +53,11 @@ public  abstract class BaicActivity  extends Activity{
 		this.requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);// 设置标题栏为用户自定义标题栏
 		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 		sctnApp = SctnAplication.getInstance();
+//		是否加密
+		encryptUsable = sharedPreferences.getString("encryptUsable", "");
+//		if(cache==null) {
+//			cache=new  CacheProcess();
+//		}
 	}
 	
 	public ImageView getTitleLeftButton() {
@@ -234,4 +255,69 @@ public  abstract class BaicActivity  extends Activity{
 	 * 初始化组件。用于在Activity装载时注册事件，以减少oncreate方法的庞大和冗余。
 	 */
 	protected abstract void reigesterAllEvent();
+	
+	/**
+	 * 以post方式提交数据
+	 * @param url 服务器的地址，直接写Action的地址，如tm/WoHandleAction.do?method=query
+	 * @param parameter 拼接好的json字符串
+	 * @return 服务端json字符串，当服务器端返回AppException或SysException时，得到的字符串是一个html文档
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	public String getPostHttpContent(String url, String parameter) {
+		SctnAplication app = (SctnAplication) this.getApplication();
+		//要是一个单例
+		HttpClient client = app.getHttpClient();
+
+		String serverUrl = Constant.ServerURL + url;
+		HttpPost post = new HttpPost(serverUrl);
+		post.addHeader("Content-Type", "application/json");
+		String result = null;
+		byte[] resultByteType = null;
+		String parameterEncrypted = null;
+		try {
+			if ("Y".equals(encryptUsable)) {
+				// 数据加密（请求数据不进行压缩）
+				parameterEncrypted = EncryptUtil.encryptThreeDESECB(parameter);
+			} else {
+				parameterEncrypted = parameter;
+			}
+			StringEntity resEntity = new StringEntity(parameterEncrypted,
+					"UTF-8");
+			post.setEntity(resEntity);
+			// 获取响应的结果
+			HttpResponse response = client.execute(post);
+			// 获取HttpEntity
+			HttpEntity respEntity = response.getEntity();
+
+			// 获取响应的结果信息
+			resultByteType = EntityUtils.toByteArray(respEntity);
+			
+			if(resultByteType == null || resultByteType.length==0){
+				return result;
+			}
+			
+			// 解压返回的数据
+			result = StringUtil.unCompress(resultByteType);
+			if ("Y".equals(encryptUsable)) {
+				// 解密返回的数据
+				result = EncryptUtil.decryptThreeDESECB(result);
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+			result = StringUtil.getAppException4MOS("网络连接异常，请检查网络设置！");
+		} catch (IOException e) {
+			e.printStackTrace();
+			result = StringUtil.getAppException4MOS("网络连接异常，请检查网络设置！");
+		} catch (ParseException e) {
+			e.printStackTrace();
+			result = StringUtil.getAppException4MOS("返回结果解析错误！");
+		} catch (SysException e) {
+			e.printStackTrace();
+			result = StringUtil.getAppException4MOS("解压或解密出现异常！");
+		}
+		return result;
+	}
+
+	
 }
